d10e4cc4f323a8a63895a032cbadf834290f096e
Lucas Brandt <lucasrbrandt@gmail.com>
2019-11-07 13:08:59 -0500

    added base endpoint and mvvm support

--- app/src/main/java/com/lucasbrandt/movieselector/MainActivity.kt
+++ app/src/main/java/com/lucasbrandt/movieselector/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.lucasbrandt.movieselector
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.lucasbrandt.movieselector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        binding.viewModel = mainActivityViewModel
     }
 }
